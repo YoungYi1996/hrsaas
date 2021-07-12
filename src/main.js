d4e9a8ad0a9c26d53d95888f8bd14e00bfa07431
Young <912624251@qq.com>
2021-07-12 12:27:15 +0800

    完成了员工管理功能

--- src/main.js
+++ src/main.js
@@ -11,8 +11,9 @@ import '@/styles/index.scss' // global css
 import App from './App'
 import store from './store'
 import router from './router'
-
+import Components from '@/components/index'
 import * as directives from '@/directives/index'
+import * as filters from '@/filters'
 import '@/icons' // icon
 import '@/permission' // permission control
 
@@ -36,6 +37,13 @@ Vue.use(ElementUI, { locale })
 Object.keys(directives).forEach(key => {
   Vue.directive(key, directives[key]) // 注册自定义指令
 })
+
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key]) // 注册自定义过滤器
+})
+
+// 注册自定义组件
+Vue.use(Components)
 
 Vue.config.productionTip = false
 
--- src/router/modules/employees.js
+++ src/router/modules/employees.js
@@ -6,13 +6,23 @@ export default {
   path: '/employees', // 路由地址
   name: 'employees', // 给模块一级路由加name属性
   component: Layout,
-  children: [{
-    path: '',
-    component: () => import('@/views/employees'),
-    // 路由元信息 其实就是一个存储数据的地方，可以放任何内容
-    meta: {
-      title: '员工管理', // 左侧导航读取了这里的title属性
-      icon: 'people'
+  children: [
+    {
+      path: '',
+      component: () => import('@/views/employees'),
+      // 路由元信息 其实就是一个存储数据的地方，可以放任何内容
+      meta: {
+        title: '员工管理', // 左侧导航读取了这里的title属性
+        icon: 'people'
+      }
+    },
+    {
+      path: 'detail/:id?', // 动态路由参数
+      component: () => import('@/views/employees/detail'),
+      hidden: true, // 不在左侧菜单显示
+      meta: {
+        title: '员工详情' // 标记当前路由规则的中文名称 后续在做左侧菜单时 使用
+      }
     }
-  }]
+  ]
 }
